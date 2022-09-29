ea97cc45b21c1760d19fb681f4c27f38b3e1e3e5
Raihan567 <aburaihansarker567@gmail.com>
2022-09-29 10:35:21 +0600

    redux toolkit counter app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CounterView from "./app/features/counter/CounterView";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter application</h1>
+      <CounterView />
+    </div>
+  );
+}
+
+export default App;
